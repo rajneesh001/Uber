3927af08ac6e7f0a9b683554ac9f9056b21738cb
Rajneesh Patwa <rajneeshpatwa@gmail.com>
2020-07-10 23:37:12 +0530

    fix in url endpoint

--- uber/cabSharing/urls.py
+++ uber/cabSharing/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [
     url(r'^request/$', views.RequestRide.as_view(), name='request_ride'),
     url(r'^rideStatus/(?P<pk>[0-9]+)/$', views.RideStatus.as_view(), name='rideStatus'),
-    url(r'^Rider/$', views.Rider.as_view(), name='rider_list'),
+    url(r'^rider/$', views.Rider.as_view(), name='rider_list'),
     url(r'^driver/$', views.Driver.as_view(), name='driver_list')
 ]
